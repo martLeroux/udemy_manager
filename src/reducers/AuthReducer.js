44f56e65e67db51f114baf3dcc38b71abebcef6b
Leroux Martin <tintin_151@hotmail.com>
2018-06-11 15:23:22 -0400

    Connects the password to redux

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,6 +1,12 @@
-import { EMAIL_CHANGED } from '../actions/types';
+import {
+  EMAIL_CHANGED,
+  PASSWORD_CHANGED
+} from '../actions/types';
 
-const INITIAL_STATE = { email: '' };
+const INITIAL_STATE = {
+  email: '',
+  password: '' 
+};
 
 export default(state = INITIAL_STATE, action) => {
   switch (action.type) {
@@ -11,6 +17,8 @@ export default(state = INITIAL_STATE, action) => {
       return { ...state, email: action.payload };
       // we need to create a new object, take all the element
       //in my previous state and replace the e-mail one
+    case PASSWORD_CHANGED:
+        return { ...state, password: action.payload };
     default:
       return state;
   }
