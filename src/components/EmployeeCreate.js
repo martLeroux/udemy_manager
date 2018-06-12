61a75d4c0ebcb8323c4fc632fc2e96ce7afe9dd4
Leroux Martin <tintin_151@hotmail.com>
2018-06-12 14:29:17 -0400

    Import component

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { Card, CardSection, Input, Button } from './';
 
 
 class EmployeeCreate extends Component {
