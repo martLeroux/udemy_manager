c3ba81d2065cd70b385d69779efbd11c9b74ef72
Leroux Martin <tintin_151@hotmail.com>
2018-10-14 09:23:22 -0400

    Broken

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,8 +1,9 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { View, Text, ListView } from 'react-native';
+import { ListView } from 'react-native';
 import { employeesFetch } from '../actions/EmployeeActions';
+import ListItem from './ListItem';
 
 class EmployeeList extends Component {
   componentWillMount() {
@@ -27,18 +28,24 @@ class EmployeeList extends Component {
     this.dataSource = ds.cloneWithRows(employees);
   }
 
+  renderRow(employee) {
+    return <ListItem employee={employee} />;
+  }
+
   render() {
     return (
-      <View>
-        <Text>Employee List</Text>
-      </View>
+      <ListView
+        enableEmptySections
+        dataSource={this.dataSource}
+        renderRow={this.renderRow}
+      />
     );
   }
 }
 
 const mapStateToProps = state => {
   const employees = _.map(state.employees, (val, uid) => {
-    return { ...val, uid };
+    return { ...val, uid }; // { shift: 'Monday', name: 'X'}
   });
 };
 
